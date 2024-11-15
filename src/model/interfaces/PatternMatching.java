c15daad3db741fb75160fcc52eea9d412fc668ad
André Luís <andreluis2442@gmail.com>
2024-11-14 23:31:18 -0300

    Enhance PatternController to manage active pattern matching and handle IOException in search method

--- src/model/interfaces/PatternMatching.java
+++ src/model/interfaces/PatternMatching.java
@@ -1,9 +1,10 @@
 package model.interfaces;
 
+import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.util.List;
 import java.util.Optional;
 
 public interface PatternMatching {
-	public Optional<List<Long>> search(RandomAccessFile file, byte[] pattern);
+	public Optional<List<Long>> search(RandomAccessFile file, byte[] pattern) throws IOException;
 }
